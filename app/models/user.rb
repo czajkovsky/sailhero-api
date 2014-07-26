722095075db0b2f4d65cc3baa48eeec0d2b1b466
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-07-26 16:05:00 +0200

    create auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save :encrypt_password
+  before_create :set_auth_token
 
   attr_accessor :password
 
@@ -16,4 +17,17 @@ class User < ActiveRecord::Base
     end
   end
 
+  private
+
+  def set_auth_token
+    return if auth_token.present?
+    self.auth_token = generate_auth_token
+  end
+
+  def generate_auth_token
+    loop do
+      token = SecureRandom.hex
+      break token unless self.class.exists?(auth_token: token)
+    end
+  end
 end
