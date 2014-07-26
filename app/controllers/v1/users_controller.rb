3f18b2d45273f1cdca1d87a127ba075a6fb74d33
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-07-26 14:05:11 +0200

    setup app config

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,7 @@
+class V1::UsersController < V1::VersionController
+
+  def index
+    respond_with :json, body: 'Not implemented yet'
+  end
+
+end
---
+++ app/controllers/v1/version_controller.rb
@@ -0,0 +1,4 @@
+class V1::VersionController < ApplicationController
+  abstract!
+
+end
