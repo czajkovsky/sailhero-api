d14f58b4bdbaf062d0c3848e541f0253f42aa241
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-07-27 13:28:40 +0200

    add serialiser to places

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  serialization_scope :view_context
   respond_to :json
 
   decent_configuration do
---
+++ app/serializers/place_serializer.rb
@@ -0,0 +1,3 @@
+class PlaceSerializer < ActiveModel::Serializer
+  attributes :id, :name, :created_at
+end
