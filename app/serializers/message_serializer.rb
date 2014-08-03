75904bf0bd526b28366061e1d10e45e4ddf1728e
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-08-03 18:07:14 +0200

    render message author

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,12 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :body, :created_at
+  attributes :id, :body, :created_at, :author
+
+  def author
+    user = object.user
+    { id: user.id,
+      email: user.email,
+      name: user.name,
+      surname: user.surname
+    }
+  end
 end
