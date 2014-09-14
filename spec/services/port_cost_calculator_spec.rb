6db0f94f73bab30c7669d20597136d46a2d7f3ae
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-09-14 16:55:00 +0200

    no place for big yacht

--- spec/services/port_cost_calculator_spec.rb
+++ spec/services/port_cost_calculator_spec.rb
@@ -3,7 +3,7 @@ require './app/services/port_cost_calculator'
 
 describe PortCostCalculator do
 
-  subject { PortCostCalculator.new(yacht, port) }
+  subject { PortCostCalculator.new(yacht: yacht, port: port) }
 
   let(:port) { create(:port) }
   let(:yacht) { create(:yacht) }
@@ -14,5 +14,14 @@ describe PortCostCalculator do
       expect(subject.total_cost).to eq(110)
     end
 
+    context 'for big yacht' do
+
+      subject { PortCostCalculator.new(yacht: big_yacht, port: port) }
+      let(:big_yacht) { create(:yacht, length: 10_000) }
+
+      it 'has no place' do
+        expect(subject.available).to be_falsey
+      end
+    end
   end
 end
