0786dd7c97125a9d2ca79c2b5c3a2cab4a65ffa5
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-09-14 18:15:42 +0200

    unsuccessful response

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -7,7 +7,7 @@ describe V1::UsersController, type: :controller do
     describe 'GET#me' do
       it 'denies access with 401 status code' do
         get :me
-        # expect(response).to be_fail
+        expect(response).not_to be_success
         expect(response).to have_http_status(401)
       end
     end
