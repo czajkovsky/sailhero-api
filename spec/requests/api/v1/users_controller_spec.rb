1fe1c9fe57aeb406a1111bd35aeaa41d69d5ef8e
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-09-21 13:48:54 +0200

    test creating user

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -27,4 +27,23 @@ describe V1::UsersController, type: :controller do
       end
     end
   end
+
+  describe 'POST#create' do
+    let(:user_params) { FactoryGirl.attributes_for(:user) }
+    let(:wrong_user_params) do
+      FactoryGirl.attributes_for(:user, email: 'wrong@te')
+    end
+
+    it 'creates user with 201 status' do
+      post :create, user: user_params
+      expect(response).to have_http_status(201)
+      expect(User.last.email).to eq(user_params[:email])
+    end
+
+    it 'does not create user because of wrong params' do
+      post :create, user: wrong_user_params
+      expect(response).to have_http_status(422)
+      expect(User.count).to eq(0)
+    end
+  end
 end
