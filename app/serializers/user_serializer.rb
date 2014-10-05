a8e813eda34eba727598fcf5d761aa6115b151cf
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-10-05 17:25:35 +0200

    add last position to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :surname, :created_at, :updated_at, :email
+  attributes :id, :name, :surname, :created_at, :updated_at, :email,
+             :last_position
   has_one :yacht, embed: :id, include: true
+
+  def last_position
+    { latitude: object.latitude, longitude: object.latitude,
+      updated_at: object.position_updated_at }
+  end
 end
