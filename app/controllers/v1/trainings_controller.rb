40f9a9e7fc2bfc3676cd8185f616bde4bd228554
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-10-05 20:13:27 +0200

    check if user has training

--- app/controllers/v1/trainings_controller.rb
+++ app/controllers/v1/trainings_controller.rb
@@ -9,8 +9,13 @@ module V1
     end
 
     def create
-      new_training = current_user.trainings.create(started_at: Time.now)
-      render status: 201, json: new_training
+      if Training.my(current_user).not_finished.count.zero?
+        new_training = current_user.trainings.create(started_at: Time.now)
+        render status: 201, json: new_training
+      else
+        render status: 403, json: { errors: [
+          I18n.t('active_record.training.errors.already_in_progress')] }
+      end
     end
   end
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,9 @@
 class Training < ActiveRecord::Base
   belongs_to :user
   has_many :checkpoints
+
+  scope :my, -> (user) { where(:user_id == user.id) }
+  scope :newest, -> { order('created_at DESC') }
+  scope :not_finished, -> { where(finished_at: nil) }
+  scope :finished, -> { where.not(finished_at: nil) }
 end
