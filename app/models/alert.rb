f43bf7b62d40ddfb3bf7047c352152b97604bba0
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-10-11 16:45:26 +0200

    validates alert

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,3 +1,10 @@
 class Alert < ActiveRecord::Base
   belongs_to :user
+
+  LAT_LANG_FORMAT = /\A[0-9]+\.[0-9]+\Z/i
+  ALERT_TYPES = %w( BAD_WEATHER_CONDITIONS CLOSED_AREA YACHT_FAILURE ).freeze
+
+  validates :latitude, :longitude, format: { with: LAT_LANG_FORMAT }
+  validates :latitude, :longitude, :alert_type, presence: true
+  validates :alert_type, inclusion: { in: ALERT_TYPES }
 end
