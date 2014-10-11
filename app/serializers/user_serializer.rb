a48875c2ce0d63effb9bf8c358d24177c2d7bfe4
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-10-11 14:02:23 +0200

    add region, include yacht

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :surname, :created_at, :updated_at, :email,
-             :last_position, :region_id
-  has_one :yacht, embed: :id, include: true
+             :last_position
+  has_one :yacht, include: true
+  has_one :region, include: :true
 
   def last_position
     { latitude: object.latitude, longitude: object.latitude,
--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,27 @@ Rails.application.routes.draw do
   scope :api do
     namespace :v1 do
       scope '(:locale)', locale: /en|pl/ do
+
         resources :users do
           get 'me', on: :collection
           delete 'me', on: :collection, to: 'users#deactivate_profile'
         end
+
         resources :regions, only: [:index] do
           post 'select', on: :member, to: 'regions#select'
         end
+
         resources :messages, only: [:index, :create, :show] do
           resources :replies, only: [:create, :index]
         end
+
         resources :trainings do
           resources :checkpoints, only: [:create]
         end
+
         resources :yachts, except: [:index]
         get 'maps/:location', to: 'maps#show'
+
         namespace :map do
           resources :ports, only: [:index, :show] do
             get 'calculate', on: :member
