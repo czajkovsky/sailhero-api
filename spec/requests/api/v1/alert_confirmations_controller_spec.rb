ac0aae987487ef151436fa2713cddaaeeea403ee
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-10-12 18:16:57 +0200

    add alert specs

--- spec/requests/api/v1/alert_confirmations_controller_spec.rb
+++ spec/requests/api/v1/alert_confirmations_controller_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 describe V1::AlertConfirmationsController, type: :controller do
 
   let(:region) { create(:region) }
-  let(:user) { create(:user, region_id: region.id) }
-  let(:alert) { create(:alert, user_id: user.id) }
+  let(:reporter) { create(:user, region_id: region.id) }
+  let(:confirmer) { create(:user, region_id: region.id, email: 'b@test.com') }
+  let(:confirmer2) { create(:user, region_id: region.id, email: 'c@test.com') }
+  let(:alert) { create(:alert, user_id: reporter.id) }
 
   context 'for unauthenticated user' do
 
@@ -26,16 +28,53 @@ describe V1::AlertConfirmationsController, type: :controller do
   context 'user is authenticated' do
 
     let(:app) { create_client_app }
-    let(:token) { access_token(app, user) }
-    let(:alert_params) { FactoryGirl.attributes_for(:alert) }
+    let(:reporter_token) { access_token(app, reporter) }
+    let(:confirmer_token) { access_token(app, confirmer) }
+    let(:confirmer2_token) { access_token(app, confirmer2) }
 
     describe 'POST#create' do
 
       it 'blocks self confirmation with 403 status code' do
-        post :create, id: alert, access_token: token.token
+        post :create, id: alert, access_token: reporter_token.token
         alert.reload
         expect(alert.credibility).to eq(0)
         expect(response).to have_http_status(403)
+      end
+
+      it 'up votes alert' do
+        controller.stub(:doorkeeper_token) { confirmer_token }
+        post :create, id: alert
+        controller.stub(:doorkeeper_token) { confirmer2_token }
+        post :create, id: alert
+        alert.reload
+        expect(alert.credibility).to eq(2)
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'DELETE#destroy' do
+
+      it 'blocks self confirmation with 403 status code' do
+        delete :destroy, id: alert, access_token: reporter_token.token
+        alert.reload
+        expect(alert.credibility).to eq(0)
+        expect(response).to have_http_status(403)
+      end
+
+      it 'down votes alert' do
+        delete :destroy, id: alert, access_token: confirmer_token.token
+        alert.reload
+        expect(alert.credibility).to eq(-1)
+      end
+
+      it 'down removes previous vote for alert' do
+        controller.stub(:doorkeeper_token) { confirmer_token }
+        post :create, id: alert # 1
+        delete :destroy, id: alert # -1
+        controller.stub(:doorkeeper_token) { confirmer2_token }
+        post :create, id: alert # 0
+        alert.reload
+        expect(alert.credibility).to eq(0)
       end
     end
   end
