bde3930459ad04bc824543b975cf5a44e5030b85
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-11-02 17:54:19 +0100

    device validations

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,9 @@
 class Device < ActiveRecord::Base
   belongs_to :user
+
+  DEVICE_TYPES = %w( ANDROID ).freeze
+
+  validates :device_type, inclusion: { in: DEVICE_TYPES }
+  validates :name, :key, :device_type, presence: true
+  validates :key, uniqueness: true
 end
