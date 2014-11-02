7350eef4abb1df060d473f31519a379a8f2c245e
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-11-02 18:23:41 +0100

    add wrong alert specs

--- spec/requests/api/v1/alerts_controller_spec.rb
+++ spec/requests/api/v1/alerts_controller_spec.rb
@@ -22,6 +22,7 @@ describe V1::AlertsController, type: :controller do
     let(:app) { create_client_app }
     let(:token) { access_token(app, user) }
     let(:alert_params) { FactoryGirl.attributes_for(:alert) }
+    let(:wrong_alert) { FactoryGirl.attributes_for(:alert, alert_type: 'BAD') }
 
     describe 'POST#create' do
       it 'renders OK response' do
@@ -29,6 +30,13 @@ describe V1::AlertsController, type: :controller do
         expect(Alert.first.user_id).to eq(user.id)
         expect(response).to be_success
         expect(response).to have_http_status(201)
+      end
+
+      it 'renders 422 for wrong params' do
+        post :create, alert: wrong_alert, access_token: token.token
+        expect(Alert.count).to eq(0)
+        expect(response).not_to be_success
+        expect(response).to have_http_status(422)
       end
     end
   end
