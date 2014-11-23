d761031f5393c87207dc646b90415d2403762bd0
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-11-23 01:00:42 +0100

    add specs for region specific alerts

--- spec/requests/api/v1/alerts_controller_spec.rb
+++ spec/requests/api/v1/alerts_controller_spec.rb
@@ -41,12 +41,26 @@ describe V1::AlertsController, type: :controller do
     end
 
     describe 'GET#index' do
+      let(:second_region) { create(:region) }
+      let(:second_user) do
+        create(:user, region_id: second_region.id, email: 'another@email.com')
+      end
+      let(:second_token) { access_token(app, second_user) }
+      let(:alert_params) { FactoryGirl.attributes_for(:alert) }
+
       it 'renders OK response' do
         post :create, alert: alert_params, access_token: token.token
         get :index
         expect(response).to be_success
         expect(JSON.parse(response.body)['alerts'].count).to eq(1)
         expect(response).to have_http_status(200)
+      end
+
+      it 'renders only region specific alerts' do
+        post :create, alert: alert_params, access_token: token.token
+        post :create, alert: alert_params, access_token: second_token.token
+        get :index
+        expect(JSON.parse(response.body)['alerts'].count).to eq(1)
       end
     end
   end
