415cf18bcea297d90729558af073d39ce8a624a1
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-11-23 15:36:51 +0100

    add friendship states methods

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,16 @@ class Friendship < ActiveRecord::Base
   belongs_to :friend, class_name: 'User'
 
   STATES = %w( pending accepted blocked ).freeze
+
+  def accepted?
+    status == 1
+  end
+
+  def pending?
+    status == 0
+  end
+
+  def blocked?
+    status == 2
+  end
 end
