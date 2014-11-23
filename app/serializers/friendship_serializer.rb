117c528342d0eae44af1c8c24844db84bb0a150a
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-11-23 13:44:09 +0100

    add friendship serializer

---
+++ app/serializers/friendship_serializer.rb
@@ -0,0 +1,21 @@
+class FriendshipSerializer < ActiveModel::Serializer
+  attributes :id, :user, :friend, :created_at, :updated_at
+
+  def user
+    side(object.user_id)
+  end
+
+  def friend
+    side(object.friend_id)
+  end
+
+  def side(id)
+    user = User.find(id)
+    {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      surname: user.surname
+    }
+  end
+end
