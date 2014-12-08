0e8c418287e2eb27123a1cc1141c7657edea8570
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-08 20:41:26 +0100

    override device

--- app/controllers/v1/devices_controller.rb
+++ app/controllers/v1/devices_controller.rb
@@ -2,6 +2,7 @@ module V1
   class DevicesController < VersionController
     doorkeeper_for :all
     expose(:device, attributes: :permitted_params)
+    before_action :device_exists?
 
     def create
       if device.save
@@ -13,8 +14,17 @@ module V1
       end
     end
 
+    private
+
     def permitted_params
       params.require(:device).permit(:device_type, :name, :key)
+    end
+
+    def device_exists?
+      user_device = Device.where(key: params[:key]).first
+      return unless user_device
+      user_device.update_attributes(token_id: doorkeeper_token.id)
+      render status: 201, json: user_device
     end
   end
 end
--- spec/requests/api/v1/devices_controller_spec.rb
+++ spec/requests/api/v1/devices_controller_spec.rb
@@ -23,6 +23,7 @@ describe V1::DevicesController, type: :controller do
 
     let(:app) { create_client_app }
     let(:token) { access_token(app, user) }
+    let(:token2) { access_token(app, user) }
 
     describe 'POST#create' do
       it 'renders OK response' do
@@ -32,6 +33,16 @@ describe V1::DevicesController, type: :controller do
         expect(device.token_id).to eq(token.id)
         expect(response).to be_success
         expect(response).to have_http_status(201)
+      end
+
+      it 'overrides existing device' do
+        controller.stub(:doorkeeper_token) { token }
+        post :create, device: device_params
+        controller.stub(:doorkeeper_token) { token2 }
+        post :create, device: device_params
+        device = Device.first
+        expect(device.user_id).to eq(user.id)
+        expect(device.token_id).to eq(token2.id)
       end
 
       it 'it allows only android device' do
