ae98c52b439083e42900258835ad1f536f7d8ca5
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-08 19:49:19 +0100

    destroy device

--- app/controllers/v1/doorkeeper/tokens_controller.rb
+++ app/controllers/v1/doorkeeper/tokens_controller.rb
@@ -2,6 +2,7 @@ module V1
   module Doorkeeper
     class TokensController < ::Doorkeeper::TokensController
       def revoke
+        Device.where(token_id: doorkeeper_token.id).destroy_all
         super
       end
     end
