5baba76176bdece5848b01d4884389c072e2aa94
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-09 22:26:38 +0100

    add store directory

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,6 +2,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
   process :set_content_type
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   def filename
     "#{timestamp}-#{super}"
   end
