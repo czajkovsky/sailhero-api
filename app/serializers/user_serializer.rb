50e34ab803cdb9eeb13efa809256500790594236
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-09 23:08:27 +0100

    include avatar url in user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :surname, :created_at, :updated_at, :email,
-             :last_position
+             :last_position, :avatar_url
   has_one :yacht, include: true
   has_one :region, include: :true
 
   def last_position
     { latitude: object.latitude, longitude: object.latitude,
       updated_at: object.position_updated_at }
+  end
+
+  def avatar_url
+    object.avatar.url
   end
 end
