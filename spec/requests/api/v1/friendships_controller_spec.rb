5db87c684326d9299754a50527ed9ace98770f3c
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-14 18:39:54 +0100

    refactor friendship specs

--- spec/requests/api/v1/friendships_controller_spec.rb
+++ spec/requests/api/v1/friendships_controller_spec.rb
@@ -139,15 +139,27 @@ describe V1::FriendshipsController, type: :controller do
     end
 
     describe 'GET#index' do
-      it 'resonds with all friendships' do
-        controller.stub(:doorkeeper_token) { friend_token }
-        post :create, friend_id: user.id, friendship: { friend_id: user.id }
-        controller.stub(:doorkeeper_token) { token }
-        get :index
-        expect(response).to be_success
-        expect(JSON.parse(response.body)['sent'].count).to eq(0)
-        expect(JSON.parse(response.body)['accepted'].count).to eq(0)
-        expect(JSON.parse(response.body)['pending'].count).to eq(1)
+      context 'gets list of all friends' do
+        before do
+          controller.stub(:doorkeeper_token) { friend_token }
+          post :create, friend_id: user.id, friendship: { friend_id: user.id }
+          controller.stub(:doorkeeper_token) { token }
+          get :index
+        end
+
+        it_behaves_like 'a successful request'
+
+        it 'includes sent invites' do
+          expect(json.sent.count).to eq(0)
+        end
+
+        it 'includes pending invites' do
+          expect(json.pending.count).to eq(1)
+        end
+
+        it 'includes accepted invites' do
+          expect(json.accepted.count).to eq(0)
+        end
       end
     end
 
@@ -158,18 +170,30 @@ describe V1::FriendshipsController, type: :controller do
       let(:third_user) { create(:user, email: 't3@example.com') }
       let(:third_token) { access_token(app, third_user) }
 
-      it 'cancels friendship' do
-        controller.stub(:doorkeeper_token) { token }
-        post :cancel, id: friendship
-        expect(response).to have_http_status(200)
-        expect(Friendship.count).to eq(0)
+      context 'cancels friendship' do
+        before do
+          controller.stub(:doorkeeper_token) { token }
+          post :cancel, id: friendship
+        end
+
+        it_behaves_like 'a successful request'
+
+        it 'removes friendship' do
+          expect(Friendship.count).to eq(0)
+        end
       end
 
-      it 'only allows owners to destroy friendship' do
-        controller.stub(:doorkeeper_token) { third_token }
-        post :cancel, id: friendship
-        expect(response).to have_http_status(403)
-        expect(Friendship.count).to eq(1)
+      context 'not an owner tries to cancel' do
+        before do
+          controller.stub(:doorkeeper_token) { third_token }
+          post :cancel, id: friendship
+        end
+
+        it_behaves_like 'a forbidden request'
+
+        it "doesn't remove friendship" do
+          expect(Friendship.count).to eq(1)
+        end
       end
     end
 
@@ -178,13 +202,22 @@ describe V1::FriendshipsController, type: :controller do
         create(:friendship, user_id: user.id, friend_id: friend.id)
       end
 
-      it 'responds with friendship' do
-        controller.stub(:doorkeeper_token) { token }
-        get :show, id: friendship
-        expect(response).to have_http_status(200)
-        body = JSON.parse(response.body)['friendship']
-        expect(body['friend']['id']).to eq(friend.id)
-        expect(body['invited']).to eq(false)
+      context 'gets friendship' do
+        before do
+          controller.stub(:doorkeeper_token) { token }
+          get :show, id: friendship
+        end
+
+        it_behaves_like 'a successful request'
+
+        it 'responds with correct friend' do
+          expect(json.friendship.friend.id).to eq(friend.id)
+        end
+
+        it 'sets user as inviter' do
+          expect(json.friendship.invited).to eq(false)
+        end
+
       end
     end
 
@@ -193,32 +226,44 @@ describe V1::FriendshipsController, type: :controller do
         create(:friendship, user_id: user.id, friend_id: friend.id)
       end
 
-      it 'it denies access for user' do
-        controller.stub(:doorkeeper_token) { token }
-        post :deny, id: friendship
-        expect(response).to have_http_status(403)
-        expect(user.friendships.count).to eq(1)
+      context 'inviter tries to deny' do
+        before do
+          controller.stub(:doorkeeper_token) { token }
+          post :deny, id: friendship
+        end
+
+        it_behaves_like 'a forbidden request'
+
+        it "doesn't change friendships count" do
+          expect(user.friendships.count).to eq(1)
+        end
       end
 
-      it 'denies friendship' do
-        controller.stub(:doorkeeper_token) { friend_token }
-        post :deny, id: friendship
-        expect(response).to have_http_status(200)
-        expect(user.friendships.count).to eq(0)
+      context 'invited denies friendship' do
+        before do
+          controller.stub(:doorkeeper_token) { friend_token }
+          post :deny, id: friendship
+        end
+
+        it_behaves_like 'a successful request'
+
+        it 'removes friendship' do
+          expect(user.friendships.count).to eq(0)
+        end
       end
 
       it "doesn't include friendship in any list" do
         controller.stub(:doorkeeper_token) { friend_token }
         post :accept, id: friendship
         get :pending
-        expect(JSON.parse(response.body)['friendships'].count).to eq(0)
+        expect(json.friendships.count).to eq(0)
         get :accepted
-        expect(JSON.parse(response.body)['friendships'].count).to eq(1)
+        expect(json.friendships.count).to eq(1)
         controller.stub(:doorkeeper_token) { token }
         get :sent
-        expect(JSON.parse(response.body)['friendships'].count).to eq(0)
+        expect(json.friendships.count).to eq(0)
         get :accepted
-        expect(JSON.parse(response.body)['friendships'].count).to eq(1)
+        expect(json.friendships.count).to eq(1)
       end
     end
 
@@ -227,38 +272,75 @@ describe V1::FriendshipsController, type: :controller do
         create(:friendship, user_id: user.id, friend_id: friend.id)
       end
 
-      it 'it denies access for user' do
-        controller.stub(:doorkeeper_token) { token }
-        post :accept, id: friendship
-        expect(response).to have_http_status(403)
-        expect(user.friendships.count).to eq(1)
-        friendship.reload
-        expect(friendship.pending?).to eq(true)
-        expect(friendship.accepted?).to eq(false)
+      context 'user is not authorized' do
+        before do
+          controller.stub(:doorkeeper_token) { token }
+          post :accept, id: friendship
+          friendship.reload
+        end
+
+        it_behaves_like 'a forbidden request'
+
+        it 'still includes friendship' do
+          expect(user.friendships.count).to eq(1)
+        end
+
+        it 'keeps friendship as pending' do
+          expect(friendship.pending?).to eq(true)
+        end
+
+        it "doesn't mark friendship as accepted" do
+          expect(friendship.accepted?).to eq(false)
+        end
       end
 
-      it 'accepts friendship' do
-        controller.stub(:doorkeeper_token) { friend_token }
-        post :accept, id: friendship
-        expect(response).to have_http_status(200)
-        friendship.reload
-        expect(friendship.accepted?).to eq(true)
-        body = JSON.parse(response.body)['friendship']
-        expect(body['friend']['id']).to eq(user.id)
-      end
+      context 'user is authorized' do
+        before(:each) do
+          controller.stub(:doorkeeper_token) { friend_token }
+          post :accept, id: friendship
+          friendship.reload
+        end
 
-      it 'includes friendship in list for both users' do
-        controller.stub(:doorkeeper_token) { friend_token }
-        post :accept, id: friendship
-        get :accepted
-        body = JSON.parse(response.body)['friendships']
-        expect(body[0]['friend']['id']).to eq(user.id)
-        expect(body.count).to eq(1)
-        controller.stub(:doorkeeper_token) { token }
-        get :accepted
-        body = JSON.parse(response.body)['friendships']
-        expect(body.count).to eq(1)
-        expect(body[0]['friend']['id']).to eq(friend.id)
+        it_behaves_like 'a successful request'
+
+        it 'marks friendship as accepted' do
+          expect(friendship.accepted?).to eq(true)
+        end
+
+        it 'responds with friendship data' do
+          expect(json.friendship.friend.id).to eq(user.id)
+        end
+
+        context 'when checks accepted' do
+          before(:each) { get :accepted }
+
+          it_behaves_like 'a successful request'
+
+          it 'includes user as friend' do
+            expect(json.friendships.first.friend.id).to eq(user.id)
+          end
+
+          it 'responds with proper list of friendships' do
+            expect(json.friendships.count).to eq(1)
+          end
+        end
+
+        context 'when users checks same list' do
+          before(:each) do
+            controller.stub(:doorkeeper_token) { token }
+            get :accepted
+          end
+
+          it_behaves_like 'a successful request'
+
+          it 'includes user as friend' do
+            expect(json.friendships.first.friend.id).to eq(friend.id)
+          end
+
+          it 'responds with proper list of friendships' do
+            expect(json.friendships.count).to eq(1)
+          end
+        end
       end
     end
   end
--- spec/support/jsons_helper.rb
+++ spec/support/jsons_helper.rb
@@ -1,7 +1,7 @@
 module Requests
   module JsonsHelpers
     def json
-      @json ||= Hashie::Mash.new(JSON.parse(response.body))
+      @json = Hashie::Mash.new(JSON.parse(response.body))
     end
   end
 end
