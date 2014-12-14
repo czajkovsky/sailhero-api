909dfee81eee7e23a45007b97866de288893c58e
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-14 23:29:15 +0100

    store in file for dev and test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,16 @@
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV['S3_BUCKET']
-  config.aws_acl    = :public_read
-  config.store_dir  = nil
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+  if Rails.env.production? || Rails.env.staging?
+    config.storage    = :aws
+    config.aws_bucket = ENV['S3_BUCKET']
+    config.aws_acl    = :public_read
+    config.store_dir  = nil
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
-  config.aws_credentials = {
-    access_key_id:     ENV['S3_KEY'],
-    secret_access_key: ENV['S3_SECRET']
-  }
+    config.aws_credentials = {
+      access_key_id:     ENV['S3_KEY'],
+      secret_access_key: ENV['S3_SECRET']
+    }
+  elsif Rails.env.development? || Rails.env.test?
+    config.storage :file
+  end
 end
