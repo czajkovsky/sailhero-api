d26042600e31e07948646a2d0ad9a23160cbc799
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-16 22:58:18 +0100

    serialise share position

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :surname, :created_at, :updated_at, :email,
-             :last_position, :avatar_url
+             :last_position, :avatar_url, :share_position
   has_one :yacht, include: true
   has_one :region, include: :true
 
