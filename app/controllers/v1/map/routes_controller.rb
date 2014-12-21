83aa60cdca750fa98d21f2f7b827180da9029510
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-21 19:47:11 +0100

    render single route

--- app/controllers/v1/map/routes_controller.rb
+++ app/controllers/v1/map/routes_controller.rb
@@ -1,8 +1,21 @@
 module V1
   module Map
     class RoutesController < RegionRestrictedController
+      before_action :check_region, except: :index
+      expose(:route)
       def index
         render json: current_region.routes
+      end
+
+      def show
+        render json: route
+      end
+
+      private
+
+      def check_region
+        invalid = route.nil? || current_region.id != route.region_id
+        render nothing: true, status: 404 if invalid
       end
     end
   end
