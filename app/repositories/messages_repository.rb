db6f7199a0b2ad93ff2bd96e3e4cec00e8ba9105
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-12-26 13:52:39 +0100

    clean up limits

--- app/repositories/messages_repository.rb
+++ app/repositories/messages_repository.rb
@@ -3,8 +3,7 @@ class MessagesRepository < OpenStruct
     params[:order] = 'ASC' unless %w(ASC DESC).include?(params[:order])
     params[:sign] = (params[:order] == 'DESC' ? '>=' : '<=')
     params[:messages] = []
-    params[:limit] = [params[:limit].to_i, 100].min
-    params[:limit] = 25 if params[:limit] < 1
+    params[:limit] = normalize_limits(params[:limit])
     super params
     self.messages = fetch_messages
   end
@@ -17,6 +16,12 @@ class MessagesRepository < OpenStruct
   end
 
   private
+
+  def normalize_limits(limit)
+    limit = [limit.to_is, 100].min
+    return 25 if limit < 25
+    limits
+  end
 
   def fetch_messages
     reversed_order = (order == 'ASC' ? 'DESC' : 'ASC')
