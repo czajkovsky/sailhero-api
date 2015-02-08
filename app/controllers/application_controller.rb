e19c89dbdf7c79d7bd11bafeea55f18b6902185f
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-02-08 22:16:10 +0100

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,8 @@ class ApplicationController < ActionController::Base
   end
 
   def update_current_position
-    latitude = request.env['X-SailHero-Latitude']
-    longitude = request.env['X-SailHero-Longitude']
+    latitude = request.env['X-Sailhero-Latitude']
+    longitude = request.env['X-Sailhero-Longitude']
     return false if latitude.nil? || longitude.nil? || current_user.nil?
     current_user.update_attributes(latitude: latitude, longitude: longitude,
                                    position_updated_at: Time.now)
